df4053bdf5809fb24782e02267e1956d309408d9
med-amine-fh <fhal.mohamed.amine@gmail.com>
2020-06-07 17:49:42 +0100

    Updates

---
+++ precache-manifest.40af6e07540ee68d0ab058d647e622d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af8198978acd1bed943012b78b8d464",
+    "url": "/covidTracker/index.html"
+  },
+  {
+    "revision": "3deccba34e2f5856ba99",
+    "url": "/covidTracker/static/css/main.011cf435.chunk.css"
+  },
+  {
+    "revision": "427975c794000e37c980",
+    "url": "/covidTracker/static/js/2.47973735.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covidTracker/static/js/2.47973735.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3deccba34e2f5856ba99",
+    "url": "/covidTracker/static/js/main.74c857d7.chunk.js"
+  },
+  {
+    "revision": "42e537d3b2e23925c168",
+    "url": "/covidTracker/static/js/runtime-main.cfc9223a.js"
+  }
+]);
